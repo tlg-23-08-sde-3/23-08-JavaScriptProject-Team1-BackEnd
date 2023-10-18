369a36efbcbbb225eab71898ef896daa710694c7
Chris Covey <chris.covey@tlgcohort.com>
2023-10-18 05:47:06 -0400

    added job routes, updated job model

--- models/job.js
+++ models/job.js
@@ -6,10 +6,11 @@ TODOs: Add additional properties
 */
 const jobSchema = new mongoose.Schema(
     {
+        companyId: { type: mongoose.ObjectId, ref: "Company", required: true },
         status: { type: String, enum: ["open", "closed"], default: "open" },
         name: { type: String, required: true },
         contents: { type: String, required: true },
-        pay: { type: Number },
+        pay: { type: String },
         keywords: { type: Array },
     },
     { timestamps: { createdAt: "created_at" } }
