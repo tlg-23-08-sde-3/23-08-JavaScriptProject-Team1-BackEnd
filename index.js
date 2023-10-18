1d28f0123b4d140680bb725581355eda11fb0a99
Sarthak Oza <Sarthak.Oza@tlgcohort.com>
2023-10-17 17:10:52 -0700

    auth completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+// const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
 
@@ -11,10 +12,11 @@ const candidatesRouter = require("./routes/candidates");
 app.use(express.json()); //Middleware from express on all requests
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+// app.use(cookieParser());
 
 app.use("/candidate", candidatesRouter);
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("server listening");
-    connect();
+   console.log("server listening");
+   connect();
 });
