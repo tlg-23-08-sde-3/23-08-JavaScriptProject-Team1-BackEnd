52fbcf67c1760d6394e9a37fa9537fc7cb8f9edb
Chris Covey <chris.covey@tlgcohort.com>
2023-10-19 06:51:11 -0400

    updated jobs route for a display by company id

--- routes/jobs.js
+++ routes/jobs.js
@@ -13,15 +13,15 @@ Functionality: creates a new job
 Usecase: employer uses to create a new job from a job form
 */
 Router.post("/", async (req, res) => {
-   try {
-      const company = await Company.findById(req.body.companyId);
-      req.body.companyId = company._id;
+    try {
+        const company = await Company.findById(req.body.companyId);
+        req.body.companyId = company._id;
 
-      const newJob = await Job.create(req.body);
-      res.send(newJob);
-   } catch (error) {
-      res.status(500).send(error.message);
-   }
+        const newJob = await Job.create(req.body);
+        res.send(newJob);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 /*
@@ -31,43 +31,43 @@ Functionality: gets a job list based upon query
 Usecase:
 */
 Router.get("/", async (req, res) => {
-   if (Object.keys(req.query).length > 0) {
-      console.log(req.query);
+    if (Object.keys(req.query).length > 0) {
+        console.log(req.query);
 
-      const query = {};
+        const query = {};
 
-      // Check if keywords are provided in the query
-      if (req.query.keywords) {
-         query["keywords"] = {
-            $elemMatch: { $regex: new RegExp(req.query.keywords, "i") },
-         };
-      }
+        // Check if keywords are provided in the query
+        if (req.query.keywords) {
+            query["keywords"] = {
+                $elemMatch: { $regex: new RegExp(req.query.keywords, "i") },
+            };
+        }
 
-      // Add other query parameters as needed
-      if (req.query.location) {
-         query["location"] = req.query.location;
-      }
+        // Add other query parameters as needed
+        if (req.query.location) {
+            query["location"] = req.query.location;
+        }
 
-      if (req.query.name) {
-         query["name"] = req.query.name;
-      }
+        if (req.query.name) {
+            query["name"] = req.query.name;
+        }
 
-      try {
-         const foundJob = await Job.find(query)
-            .collation({ locale: "en", strength: 2 }) // case insensitive match
-            .exec();
+        try {
+            const foundJob = await Job.find(query)
+                .collation({ locale: "en", strength: 2 }) // case insensitive match
+                .exec();
 
-         res.send(foundJob);
-      } catch (error) {
-         res.status(500).send({ error: "Job retrieval failed" });
-      }
-   } else {
-      try {
-         res.send(await Job.find());
-      } catch (error) {
-         res.status(500).send({ error: "Job retrieval failed" });
-      }
-   }
+            res.send(foundJob);
+        } catch (error) {
+            res.status(500).send({ error: "Job retrieval failed" });
+        }
+    } else {
+        try {
+            res.send(await Job.find());
+        } catch (error) {
+            res.status(500).send({ error: "Job retrieval failed" });
+        }
+    }
 });
 
 /*
@@ -77,16 +77,35 @@ Functionality: get a job by Id
 Usecase:
 */
 Router.get("/id/:_id", async (req, res) => {
-   const { _id } = req.params;
-   try {
-      const job = await Job.findById({ _id });
-      if (!job) {
-         return res.status(404).json({ error: `No job found with ID: ${_id}` });
-      }
-      res.send(job);
-   } catch (error) {
-      res.status(500).send({ error: "Job retrieval failed" });
-   }
+    const { _id } = req.params;
+    try {
+        const job = await Job.findById({ _id });
+        if (!job) {
+            return res.status(404).json({ error: `No job found with ID: ${_id}` });
+        }
+        res.send(job);
+    } catch (error) {
+        res.status(500).send({ error: "Job retrieval failed" });
+    }
+});
+
+/*
+GET: /job/company/:companyId
+
+Functionality: get all jobs based upon companyId
+Usecase:
+*/
+Router.get("/company/:companyId", async (req, res) => {
+    try {
+        let { companyId } = req.params;
+
+        const company = await Company.findById(companyId);
+        companyId = company._id;
+        const jobs = await Job.find({ companyId }).exec();
+        res.send(jobs);
+    } catch (error) {
+        res.status(500).send({ error: "Job retrieval failed" });
+    }
 });
 
 /*
@@ -96,21 +115,21 @@ Functionality: updates a job posting based on req.body fields
 Usecase:
 */
 Router.put("/id/:_id", async (req, res) => {
-   const { _id } = req.params;
+    const { _id } = req.params;
 
-   try {
-      let updatedJob = await Job.findOneAndUpdate({ _id }, req.body, {
-         returnOriginal: false,
-      });
-      if (!updatedJob) {
-         return res.status(404).json({ error: `No job found with ID: ${_id}` });
-      }
-      res.send(updatedJob);
-   } catch (error) {
-      res.status(500).json({
-         error: `Job update failed: ${error.path} - ${error.stringValue} - ${error.messageFormat}`,
-      });
-   }
+    try {
+        let updatedJob = await Job.findOneAndUpdate({ _id }, req.body, {
+            returnOriginal: false,
+        });
+        if (!updatedJob) {
+            return res.status(404).json({ error: `No job found with ID: ${_id}` });
+        }
+        res.send(updatedJob);
+    } catch (error) {
+        res.status(500).json({
+            error: `Job update failed: ${error.path} - ${error.stringValue} - ${error.messageFormat}`,
+        });
+    }
 });
 
 /*
@@ -120,16 +139,16 @@ Functionality:
 Usecase:
 */
 Router.delete("/id/:_id", async (req, res) => {
-   const { _id } = req.params;
+    const { _id } = req.params;
 
-   try {
-      await Job.findOneAndDelete({ _id });
-      res.sendStatus(200); // Send a simple OK if successful
-   } catch (error) {
-      res.status(500).json({
-         error: `Deletion of job with an id of ${_id} failed`,
-      });
-   }
+    try {
+        await Job.findOneAndDelete({ _id });
+        res.sendStatus(200); // Send a simple OK if successful
+    } catch (error) {
+        res.status(500).json({
+            error: `Deletion of job with an id of ${_id} failed`,
+        });
+    }
 });
 
 module.exports = Router;
